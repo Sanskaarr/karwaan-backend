57d8af04d8ecb39f6e47145f8e234e3a1bd3c937
Kunal Gaur <kunaalgaurr@gmail.com>
2024-01-26 00:21:59 +0530

    updated order api

--- src/routes/order.ts
+++ src/routes/order.ts
@@ -8,6 +8,7 @@ const router = Router();
 router.route('/').post(verifyToken, verifyCredentials, createOrder);
 router.route('/:id').put(verifyToken, verifyCredentials, updateOrderPaymentStatus);
 router.route('/checkout/:id').put(verifyToken, verifyCredentials, updateOrderPaymentStatus);
-router.route('/all-orders/:id').put(verifyToken, verifyCredentials, getAllOrders);
+// THIS ID IS USER ID 
+router.route('/all-orders/:id').get(verifyToken, verifyCredentials, getAllOrders);
 
 export default router;
